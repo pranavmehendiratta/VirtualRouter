452dde4a8c14cf7b77d41a7aab892ff7b72ecf17
Pranav Mehendiratta <mehendiratta@wisc.edu>
2018-11-08 13:20:46 -0800

    Remove commented code and print statements

--- src/edu/wisc/cs/sdn/vnet/rt/ArpQO.java
+++ src/edu/wisc/cs/sdn/vnet/rt/ArpQO.java
@@ -5,36 +5,19 @@ import net.floodlightcontroller.packet.*;
 import java.util.*;
 import java.util.concurrent.*;
 
-class ArpQOData implements Runnable{ // implements Runnable {
+class ArpQOData implements Runnable {
     boolean request;
     int count;   
     Queue<Ethernet> packets;
     Iface outIface;
     Ethernet arpRequest;
     private Router router;
-    //Thread attemptThread;
-
-    // Thread setup parameters
-    //Router router;
-    //Ethernet arpRequest;
-    //Iface iface;
-
-    //public void initThreadParamters(Router router, Ethernet arpRequest, Iface inIface) {
-    //    this.router = router;
-    //    this.arpRequest = arpRequest;
-    //    this.iface = inIface;
-    //}
-
-    //public void run() {
-    //    System.out.println("ArpQOData thread execute function");	
-    //}
 
     ArpQOData(Router router) {
         request = false;
         count = 0;
         packets = new ConcurrentLinkedQueue<>();
         this.router=router;
-        //attemptThread = new Thread(this);
     }
 
     public void printPacketQueue() {
@@ -74,7 +57,7 @@ class ArpQOData implements Runnable{ // implements Runnable {
 
             // If no entry matched, do nothing
             if (null == bestmatch) { 
-                System.out.println("Cannot find the source address in the route table");
+                //System.out.println("Cannot find the source address in the route table");
                 return; 
             }   
 
@@ -91,21 +74,8 @@ class ArpQOData implements Runnable{ // implements Runnable {
     }
 }
 
-//class DataThreadObject extends ArpQOData {
-//    Thread attemptThread;
-//
-//}
-
-public class ArpQO { // implements Runnable {
+public class ArpQO { 
     static ConcurrentHashMap<Integer, ArpQOData> packetMap;
-    //ConcurrentHashMap<Integer, Thread> threadMap;
-
-//    public ArpQO() {
-//        packetMap = new ConcurrentHashMap<>();
-//        //attemptThread = new Thread(this);
-//        // this.router = r;
-//    }
-
     static public void insert(int ip, Ethernet etherPacket, Ethernet ether, Iface bestMatch, Router router) {
         if(packetMap == null)
             packetMap = new ConcurrentHashMap<>();
@@ -120,110 +90,6 @@ public class ArpQO { // implements Runnable {
         data.run();
     }
 
-    //    public void execute1(int ip, Ethernet ether, Iface inIface) {
-    //        // start the thead for ip ArpQOData object
-    //        final ArpQOData data = packetMap.get(ip);
-    //
-    //
-    //        //data.initThreadParamters(router, ether, inIface);
-    //        //data.attemptThread.start();
-    //    }
-    //
-    //    public void Run() {
-    // TODO: Figure out how to send ip here to get the data object
-
-    //final ArpQOData data = packetMap.get(ip);
-    //
-    //// Thread will stop after 3 tries
-    //while (count < 4) {
-    //    System.out.println("ARP request attempt: " + data.count);
-    //    
-    //    
-    //   
-    //    
-
-    //    // Send ARP request every seonds
-    //    count++;
-    //    try {
-    //	Thread.sleep(1000); 
-    //    } catch (InterruptedException e) {
-    //	
-    //    }
-    //}
-
-    // If request for IP is still not fulfilled remove it
-    // remove from packetsMap
-//}
-
-/*
-
-    public void timeout(final int ip, final Router router, final Ethernet ether, final Iface inIface) {
-
-	if (data.request) {
-	    System.out.println("exiting in excute for ip: " + IPv4.fromIPv4Address(ip));
-	    return;
-	}
-
-	if (data.count >= 3 && !data.request) {
-	    // Desitnation net unreachable message if even after 3 requests
-	    // mac address is not available
-
-	    //data.printPacketQueue();
-
-	    Ethernet packet = data.packets.remove();
-
-	    System.out.println("packet being sent for ICMP request is");
-	    System.out.println(packet.toString());
-
-	    IPv4 ipPacket = (IPv4)packet.getPayload();
-	    int srcAddr = ipPacket.getSourceAddress();
-	    RouteEntry bestmatch = router.getRouteTable().lookup(srcAddr);
-
-	    // If no entry matched, do nothing
-	    if (null == bestmatch) { 
-		System.out.println("Cannot find the source address in the route table");
-		return; 
-	    }   
-
-	    Iface outIface = bestmatch.getInterface();
-
-	    router.createICMPMessage(packet, outIface, (byte)3, (byte)1); 
-
-	    if (data.packets.isEmpty()) {
-		packetMap.remove(ip);
-	    }
-	    return;
-	}
-
-	new Timer().schedule(
-		new TimerTask() {
-		public void run() {
-		execute(ip, router, ether, inIface);
-		}
-		}, 1000);
-    }
-
-    public void execute (int ip, Router router, Ethernet ether, Iface inIface) {
-	ArpQOData data = packetMap.get(ip);
-
-	if (data == null) {
-	    return;
-	}
-
-	System.out.println("Attemp " + data.count  + "  at finding mac address"); 
-
-	if (data.request) {
-	    System.out.println("exiting in excute for ip: " + IPv4.fromIPv4Address(ip));
-	    return;
-	}
-
-	if (!data.request && data.count < 3) {
-	    router.sendPacket(ether, inIface);
-	    data.count++;
-	} 
-	timeout(ip, router, ether, inIface);
-    }
- */
 public static void print() {
     System.out.println("---------------- ARP Table -------------------");
     for (int ip : packetMap.keySet()) {
